526d588ef7355dbf4c1773cd92dd8c850c41d973
dimaszdn <dimaszdn@mail.ru>
2024-03-08 20:12:15 +0200

    output, addition and subtraction of numbers

--- Lab 2/code/index.php
+++ Lab 2/code/index.php
@@ -1,6 +1,6 @@
 <?php
 
-/*----- TASK 1 -----*/
+echo "----- TASK 1 -----";
 
 /* Imagine a lot of code here */
 $very_bad_unclear_name = "15 chicken wings";
@@ -11,3 +11,25 @@ $order .= " from the KFS";
 
 // Don't change the line below
 echo "\nYour order is: $very_bad_unclear_name.\n";
+
+
+echo "\n----- TASK 2 -----\n";
+
+$number1 = 52;
+echo "{$number1}\n";
+
+$number2 = 64;
+echo "{$number2}\n";
+
+$floatNumber = 3.14159;
+echo "{$floatNumber}\n";
+
+echo 6 + 6;
+echo "\n";
+
+$lastMonth = 1187.23;
+$thisMonth = 1089.98;
+echo "Разница между расходами: ";
+echo $lastMonth - $thisMonth;
+echo "\n";
+
