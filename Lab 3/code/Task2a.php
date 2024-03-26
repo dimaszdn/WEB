77b5533a86e64dc2625eb0be5ea0bf4c2556e220
dimaszdn <dimaszdn@mail.ru>
2024-03-26 23:54:26 +0200

    creating a form and counting at the click of a button

--- Lab 3/code/Task2a.php
+++ Lab 3/code/Task2a.php
@@ -1,1 +1,34 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Task2a</title>
+    <style>
+        .form-container {
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+<div class="form-container">
+    <form method="post">
+        <label for="text">Введите текст</label><br>
+        <textarea rows="3" name="text"></textarea><br>
+        <input type="submit" value="Count">
+    </form>
+    <?php
+        $text = "";
+        if (!empty($_POST['text']))
+            $text = $_POST['text'];
+        $matches = array();
+        $count = preg_match_all('/[a-zа-я0-9.]+/ui', $text, $matches);
+        $countSymbols = mb_strlen($text);
+        echo "<p>Слов в тексте: $count </p>";
+        echo "<p>Количество символов в тексте: $countSymbols</p>"
+    ?>
+</div>
+</body>
+</html>
