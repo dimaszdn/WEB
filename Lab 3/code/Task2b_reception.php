0700675e2996fa5ec0e3142533fef1100f189510
dimaszdn <dimaszdn@mail.ru>
2024-03-27 15:49:22 +0200

    data output from the session

--- Lab 3/code/Task2b_reception.php
+++ Lab 3/code/Task2b_reception.php
@@ -1,1 +1,8 @@
 <?php
+session_start();
+if (!empty($_SESSION['surname']) && !empty($_SESSION['name']) && !empty($_SESSION['age']))
+{
+    echo "<p>Фамилия: {$_SESSION['surname']}</p>";
+    echo "<p>Имя: {$_SESSION['name']}</p>";
+    echo "<p>Возраст: {$_SESSION['age']}</p>";
+}
