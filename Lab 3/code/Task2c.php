b8e874242684fe7e0b3d6e13529fd90b8d3d231c
dimaszdn <dimaszdn@mail.ru>
2024-03-27 16:09:04 +0200

    creating a form

--- Lab 3/code/Task2c.php
+++ Lab 3/code/Task2c.php
@@ -1,1 +1,47 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style>
+        label {
+            margin: 10px 0;
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+            width: 250px;
+        }
+
+        form {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+    </style>
+</head>
+<body>
+<form>
+    <form method="post">
+        <label>
+            Имя
+            <input type="text" name="name">
+        </label>
+        <label>
+            Возраст
+            <input type="text" name="age">
+        </label>
+        <label>
+            Место работы
+            <input type="text" name="workplace">
+        </label>
+        <label>
+            Зарплата
+            <input type="text" name="salary">
+        </label>
+        <input type="submit">
+</form>
+</body>
+</html>
