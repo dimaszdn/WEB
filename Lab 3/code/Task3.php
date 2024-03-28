b7d5a026f6a93e32400c761a56648c983099c289
dimaszdn <dimaszdn@mail.ru>
2024-03-28 16:00:23 +0200

    creating a form and filling out categories

--- Lab 3/code/Task3.php
+++ Lab 3/code/Task3.php
@@ -1,1 +1,42 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>BulBoard</title>
+</head>
+<body>
+    <form action="save.php" method="post">
+        <label for="email">
+            Email
+            <input type="email" name="email" required>
+        </label>
+
+        <label for="category">
+            Category
+            <select name="category" required>
+                <?php
+                $categoriesDir = '/code/categories';
+                $categories = array_filter(glob($categoriesDir . '/*'), 'is_dir');
+                foreach ($categories as $category)
+                    echo "<option value='$category'>" . basename($category) . " </option>"
+                ?>
+            </select>
+        </label>
+
+        <label for="title">
+            Title
+            <input type="text" name="title" required>
+        </label>
+
+        <label for="description">
+            Description
+            <textarea rows="3" name="description"></textarea>
+        </label>
+
+        <input type="submit" value="Save">
+    </form>
+</body>
+</html>
