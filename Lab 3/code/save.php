4d574526cc720e56610f9f1b5b5b28e82cf610a3
dimaszdn <dimaszdn@mail.ru>
2024-03-28 17:14:11 +0200

    processing the post request

--- Lab 3/code/save.php
+++ Lab 3/code/save.php
@@ -1,1 +1,22 @@
 <?php
+
+function redirectToHome() : void
+{
+    header('Location: /Task3.php');
+    exit();
+}
+
+if (false === isset($_POST['email'], $_POST['category'], $_POST['title'], $_POST['description']))
+    redirectToHome();
+
+$category = $_POST['category'];
+$title = $_POST['title'];
+$description = $_POST['description'];
+
+$filePath = "categories/{$category}/{$title}.txt";
+
+if (false === file_put_contents($filePath, $description))
+    throw new Exception('Something went wrong!');
+
+chmod($filePath, 0777);
+redirectToHome();
